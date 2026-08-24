253d4807693ecb0923b67e5d1b8d58e0c6859aa0
Bernd Schröder <schroederbernd01@gmail.com>
2026-08-24 23:48:36 +0200

    Test_JSON: Add assertions, if restore and orig file have the same values

--- test/test_json.py
+++ test/test_json.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from anym.anym_json import anonymize_json_file, restore_json_anonymization
+from anym import anym_json
 
 
 @pytest.mark.dependency()
@@ -13,12 +13,20 @@ def test_json_anym():
     output_file = Path(data_dir, "anym", "json_test_data.json")
     mapping_file = Path(data_dir, "mapping", "json_mapping_test.json")
     seed = "test_seed"
-    anonymize_json_file(
+    anym_json.anonymize_json_file(
         input_json=input_file,
         output_json=output_file,
         mapping_output=mapping_file,
         seed=seed,
     )
+
+    orig_data = anym_json.load_json_file(input_file)
+    anym_data = anym_json.load_json_file(output_file)
+
+    for orig_data_point, anym_data_point in zip(orig_data, anym_data):
+        for orig_el, anym_el in zip(orig_data_point.values(), anym_data_point.values()):
+            assert orig_el != anym_el
+            assert anym_el.startswith("ANON_")
 
     assert mapping_file.exists()
 
@@ -31,8 +39,18 @@ def test_json_restore():
     output_file = Path(data_dir, "restore", "json_test_data.json")
     mapping_file = Path(data_dir, "mapping", "json_mapping_test.json")
 
-    restore_json_anonymization(
+    anym_json.restore_json_anonymization(
         input_json=input_file,
         output_json=output_file,
         mapping_input=mapping_file,
     )
+
+    orig_file = Path(data_dir, "orig", "json_test_data.json")
+    orig_data = anym_json.load_json_file(orig_file)
+    restore_data = anym_json.load_json_file(output_file)
+
+    for orig_data_point, restore_data_point in zip(orig_data, restore_data):
+        for orig_el, restore_el in zip(
+            orig_data_point.values(), restore_data_point.values()
+        ):
+            assert orig_el == restore_el
