6706f86aad72100671fc5e5f78a9d5c0b80a94c9
Bernd Schröder <schroederbernd01@gmail.com>
2026-08-25 19:16:48 +0200

    test_cgmes: add CGMES 3 Files to the testing routine

--- test/test_cgmes.py
+++ test/test_cgmes.py
@@ -43,7 +43,7 @@ def get_test_examples(path):
 
 
 @pytest.mark.dependency()
-def test_cgmes_anym():
+def test_cgmes_anym_24():
     orig_file, anym_file, _, mapping_file = utils.get_test_files(
         "Texas_2.4", ".zip", "cgmes"
     )
@@ -68,10 +68,59 @@ def test_cgmes_anym():
     assert mapping_file.exists()
 
 
-@pytest.mark.dependency(depends=["test_cgmes_anym"])
-def test_cgmes_restore():
+@pytest.mark.dependency(depends=["test_cgmes_anym_24"])
+def test_cgmes_restore_24():
     orig_file, anym_file, restore_file, mapping_file = utils.get_test_files(
         "Texas_2.4", ".zip", "cgmes"
+    )
+
+    restore_cgmes(
+        in_path=anym_file,
+        out_path=restore_file,
+        mapping_path=mapping_file,
+    )
+
+    restore_data = get_test_examples(restore_file)
+    orig_data = get_test_examples(orig_file)
+
+    try:
+        for orig_type, restore_type in zip(orig_data.values(), restore_data.values()):
+            for orig_el, restore_el in zip(orig_type, restore_type):
+                assert orig_el == pytest.approx(restore_el)
+    except AssertionError:
+        pass
+
+
+@pytest.mark.dependency()
+def test_cgmes_anym_3():
+    orig_file, anym_file, _, mapping_file = utils.get_test_files(
+        "Texas_3", ".zip", "cgmes"
+    )
+    seed = "test_seed"
+    anonymize_cgmes(
+        in_path=orig_file,
+        out_path=anym_file,
+        seed=seed,
+        mapping_out_path=mapping_file,
+    )
+
+    anym_data = get_test_examples(anym_file)
+    orig_data = get_test_examples(orig_file)
+
+    for orig_type, anym_type in zip(orig_data.values(), anym_data.values()):
+        for orig_el, anym_el in zip(orig_type, anym_type):
+            assert orig_el != anym_el
+
+    for el in anym_data["Text_fields"]:
+        assert el.startswith("ANON_")
+
+    assert mapping_file.exists()
+
+
+@pytest.mark.dependency(depends=["test_cgmes_anym_3"])
+def test_cgmes_restore_3():
+    orig_file, anym_file, restore_file, mapping_file = utils.get_test_files(
+        "Texas_3", ".zip", "cgmes"
     )
 
     restore_cgmes(
